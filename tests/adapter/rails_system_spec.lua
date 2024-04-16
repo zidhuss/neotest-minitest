bb0b6878ed30bdb84c67c4694f05a46941814c66
igillis <hegillis@gmail.com>
2024-04-16 11:26:39 -0400

    Add support for system test cases

---
+++ tests/adapter/rails_system_spec.lua
@@ -0,0 +1,75 @@
+local plugin = require("neotest-minitest")
+local async = require("nio.tests")
+
+describe("Rails System Test", function()
+  assert:set_parameter("TableFormatLevel", -1)
+  describe("discover_positions SystemTestCase", function()
+    async.it("should discover the position for the tests", function()
+      local test_path = vim.loop.cwd() .. "/tests/minitest_examples/system_test_case.rb"
+      local positions = plugin.discover_positions(test_path):to_list()
+      local expected_positions = {
+        {
+          id = test_path,
+          name = "system_test_case.rb",
+          path = test_path,
+          range = { 0, 0, 6, 0 },
+          type = "file",
+        },
+        {
+          {
+            id = "./tests/minitest_examples/system_test_case.rb::3",
+            name = "RailsSystemTest",
+            path = test_path,
+            range = { 2, 0, 5, 3 },
+            type = "namespace",
+          },
+          {
+            {
+              id = "./tests/minitest_examples/system_test_case.rb::4",
+              name = "should pass",
+              path = test_path,
+              range = { 3, 2, 4, 5 },
+              type = "test",
+            },
+          },
+        },
+      }
+      assert.are.same(positions, expected_positions)
+    end)
+  end)
+
+  describe("discover_positions ApplicationSystemTestCase", function()
+    async.it("should discover the position for the tests", function()
+      local test_path = vim.loop.cwd() .. "/tests/minitest_examples/rails_system_test.rb"
+      local positions = plugin.discover_positions(test_path):to_list()
+      local expected_positions = {
+        {
+          id = test_path,
+          name = "rails_system_test.rb",
+          path = test_path,
+          range = { 0, 0, 6, 0 },
+          type = "file",
+        },
+        {
+          {
+            id = "./tests/minitest_examples/rails_system_test.rb::3",
+            name = "RailsSystemTest",
+            path = test_path,
+            range = { 2, 0, 5, 3 },
+            type = "namespace",
+          },
+          {
+            {
+              id = "./tests/minitest_examples/rails_system_test.rb::4",
+              name = "should pass",
+              path = test_path,
+              range = { 3, 2, 4, 5 },
+              type = "test",
+            },
+          },
+        },
+      }
+      assert.are.same(positions, expected_positions)
+    end)
+  end)
+end)
---
+++ tests/minitest_examples/system_test_case.rb
@@ -0,0 +1,6 @@
+require_relative "../application_system_test_case"
+
+class RailsSystemTest < ActionDispatch::SystemTestCase
+  test "should pass" do
+  end
+end
